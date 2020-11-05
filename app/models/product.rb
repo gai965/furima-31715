382e6d32758a5555b7c8d870aded1761aab6ce8d
gai965 <kotodama51080@gmail.com>
2020-11-05 15:40:45 +0900

    バリテーションの設定およびアソシエーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product < ApplicationRecord
-  # bleongs_to :user
+  bleongs_to :user
+
+  with_options presence: true do
+    validates :name,        length: { maximum: 40 }
+    validates :explanation, length: { maximum: 1000 }
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :precture_id
+    validates :delivery_days_id
+    validates :price
+  end
 end
