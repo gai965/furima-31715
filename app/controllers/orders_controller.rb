81f1114fbf7a29d3fd20755f51360f62e014bc1e
gai965 <kotodama51080@gmail.com>
2020-11-08 21:31:36 +0900

    クライアントサイドの決済機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,13 @@
 class OrdersController < ApplicationController
+
+  def index
+    @product = Product.find(params[:product_id])
+    @order = Order.new
+  end
+
+  def create
+    
+  end
+
+  
 end
---
+++ app/javascript/card.js
@@ -0,0 +1,53 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_b4a6b6475b6e8116bf613608")
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("card_number"),
+      cvc: formData.get("card_cvc"),
+      exp_month: formData.get("card_exp_month"),
+      exp_year: `20${formData.get("card_exp_year")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+
+      document.getElementById("card_number").removeAttribute("name");
+      document.getElementById("card_cvc").removeAttribute("name");
+      document.getElementById("card_exp_month").removeAttribute("name");
+      document.getElementById("card_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    });
+  });
+};
+
+window.addEventListener("load", pay);
