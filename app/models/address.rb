11bce2b93f45d8f65b145e3028f01c3f5ed3573a
gai965 <kotodama51080@gmail.com>
2020-11-08 22:07:58 +0900

    addressモデルのバリテーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,13 @@ class Address < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+
+  with_options presence: true do
+    validates :postal_code
+    validates :precture_id, numericality: { other_than: 0, message: 'Select' }
+    validates :explanation
+    validates :city
+    validates :house_number
+    validates :phone_number
+  end
 end
